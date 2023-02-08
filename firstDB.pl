27c456fccc7b42dc6a5ae01d200d1bbf9434b0bf
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2023-02-08 09:14:56 +0000

    Add files via upload

---
+++ firstDB.pl
@@ -0,0 +1,16 @@
+
+% creating object
+%
+male(kofi).
+male(eddy).
+male(nigel).
+
+female(aline).
+female(grace).
+female(eunice).
+
+
+like(john,marry).
+like(marry, teddy).
+like(john, chocolate).
+like(marry, chocolate).
